1ff878e6b88255b99209ffc9308c3c31e7767cd8
Ludvig Lundh <ludviglund95@gmail.com>
2024-12-12 10:19:35 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { select, text, confirm, spinner } from "@clack/prompts";
+import dotenv from "dotenv";
+import chalk from "chalk";
+import fs from "fs/promises";
+import path from "path";
+
+dotenv.config();
